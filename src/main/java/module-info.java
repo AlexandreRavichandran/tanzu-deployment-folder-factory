d0a17d09ed92a0cd196497d64d07fc5112f0cefe
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2023-05-11 20:46:42 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module tanzudeployment.tanzudeploymentfolderfactory {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens tanzudeployment.tanzudeploymentfolderfactory to javafx.fxml;
+    exports tanzudeployment.tanzudeploymentfolderfactory;
+}
